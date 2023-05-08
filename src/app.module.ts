9d22ae1595e19ce1f56a7229f40bad2e0096bf1b
Gia Hai <haidao549@gmail.com>
2023-05-08 09:33:23 +0700

    connect to mongoDb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      validationSchema: configValidationSchema,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
