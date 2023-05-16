7f8bb0b7094d39590135d6e29ee9734bd8792642
Doan Anh Hien <21520227@gm.uit.edu.vn>
2023-05-16 20:41:37 +0700

    create login function

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigService } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schema/users.schemas';
+import { PassportModule } from '@nestjs/passport';
+
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+    }),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
-export class AuthModule {}
+
+export class AuthModule { }
