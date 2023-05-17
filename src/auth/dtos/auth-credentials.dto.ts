9761e74825b290af6ad23002a73a4d2e276b87fc
Gia Hai <haidao549@gmail.com>
2023-05-18 05:37:27 +0700

    adding signup function

---
+++ src/auth/dtos/auth-credentials.dto.ts
@@ -0,0 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(8) // 1 uppercase, 1 lowercase, 1 number or special character
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is too weak',
+  })
+  password: string;
+}
