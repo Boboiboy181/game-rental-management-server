7d90a93a54369f5ff00ea947ba37fb3c05ca5dd7
Gia Hai <haidao549@gmail.com>
2023-05-25 20:17:47 +0700

    creating customer schema

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCustomerDto } from './dtos/create-customer.dto';
 import { UpdateCustomerDto } from './dtos/update-customer.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Customer } from './schemas/customer.schema';
 
 @Injectable()
 export class CustomerService {
+  constructor(
+    @InjectModel('Customer')
+    private readonly customerModel: Model<Customer>,
+  ) {}
+
   create(createCustomerDto: CreateCustomerDto) {
     return 'This action adds a new customer';
   }
--- src/customer/schemas/customer.schema.ts
+++ src/customer/schemas/customer.schema.ts
@@ -1,1 +1,22 @@
-export class Customer {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Type } from 'class-transformer';
+import { HydratedDocument } from 'mongoose';
+
+export type CustomerDocument = HydratedDocument<Customer>;
+
+@Schema({ timestamps: true })
+export class Customer {
+  @Prop()
+  name: string;
+
+  @Prop()
+  phoneNumber: string;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  point: number;
+}
+
+export const CustomerSchema = SchemaFactory.createForClass(Customer);
