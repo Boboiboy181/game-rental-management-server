1a7242b1bbdcae02105d3cb0989a070e96823da2
Gia Hai <haidao549@gmail.com>
2023-05-26 19:45:33 +0700

    adding swagger api for video-game and auth

--- src/video-game/dtos/update-video-game.dto.ts
+++ src/video-game/dtos/update-video-game.dto.ts
@@ -1,14 +1,18 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateVideoGameDto } from './create-video-game.dto';
-import { IsOptional, IsEnum } from 'class-validator';
+import { IsEnum, IsOptional } from 'class-validator';
 import { VideoGameGenreEnum } from '../enums/video-game-genre.enum';
 import { VideoGameSystemEnum } from '../enums/video-game-system.enum';
-export class UpdateVideoGameDto extends PartialType(CreateVideoGameDto) {
-    @IsOptional()
-    @IsEnum(VideoGameSystemEnum)
-    system?: VideoGameSystemEnum;
+import { ApiProperty } from '@nestjs/swagger';
 
-    @IsOptional()
-    @IsEnum(VideoGameGenreEnum)
-    genre: VideoGameGenreEnum;
+export class UpdateVideoGameDto extends PartialType(CreateVideoGameDto) {
+  @ApiProperty({ enum: VideoGameSystemEnum })
+  @IsOptional()
+  @IsEnum(VideoGameSystemEnum)
+  system?: VideoGameSystemEnum;
+
+  @ApiProperty({ enum: VideoGameGenreEnum })
+  @IsOptional()
+  @IsEnum(VideoGameGenreEnum)
+  genre: VideoGameGenreEnum;
 }
