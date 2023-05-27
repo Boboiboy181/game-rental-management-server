ba907029277acca96def602f450d7c58d6f84692
Gia Hai <haidao549@gmail.com>
2023-05-27 16:41:51 +0700

    setting up cloudinary

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -4,4 +4,7 @@ export const configValidationSchema = Joi.object({
   MONGO_URI: Joi.string().required(),
   JWT_SECRET: Joi.string().required(),
   JWT_EXPIRES: Joi.string().required(),
+  CLOUDINARY_NAME: Joi.string().required(),
+  CLOUDINARY_API_KEY: Joi.string().required(),
+  CLOUDINARY_API_SECRET: Joi.string().required(),
 });
