3b3b50f42eebe9de1e8831eed1ab53425c44e3ff
Gia Hai <haidao549@gmail.com>
2023-05-30 14:53:43 +0700

    generating the rental-package schema

--- src/rental-package/rental-package.module.ts
+++ src/rental-package/rental-package.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { RentalPackageService } from './rental-package.service';
 import { RentalPackageController } from './rental-package.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RentalPackageSchema } from './schemas/rental-package.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'RentalPackage', schema: RentalPackageSchema },
+    ]),
+  ],
   controllers: [RentalPackageController],
   providers: [RentalPackageService],
 })
--- src/rental-package/schemas/rental-package.schema.ts
+++ src/rental-package/schemas/rental-package.schema.ts
@@ -1,1 +1,30 @@
-export class RentalPackage {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Schema as MongooseSchema, Types } from 'mongoose';
+import { Customer } from '../../customer/schemas/customer.schema';
+
+export type RentalPackageDocument = HydratedDocument<RentalPackage>;
+
+@Schema()
+export class RentalPackage {
+  // name of rental package
+  @Prop()
+  packageName: string;
+
+  // number of games that can be rented
+  @Prop()
+  numberOfGames: number;
+
+  // price of rental package
+  @Prop()
+  price: number;
+
+  // time of rental in days
+  @Prop()
+  timeOfRental: number;
+
+  // customer registration this rental package is associated with
+  @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'Customer' })
+  customerRegistration: Types.ObjectId[];
+}
+
+export const RentalPackageSchema = SchemaFactory.createForClass(RentalPackage);
