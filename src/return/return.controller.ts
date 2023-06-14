0af788f5ab78f2383642b6764eb20d0557c20d92
Doan Anh Hien <21520227@gm.uit.edu.vn>
2023-06-14 07:08:23 +0700

    add API tag

--- src/return/return.controller.ts
+++ src/return/return.controller.ts
@@ -12,32 +12,39 @@ import { ReturnService } from './return.service';
 import { CreateReturnDto } from './dtos/create-return.dto';
 import { UpdateReturnDto } from './dtos/update-return.dto';
 import { FilterReturnDto } from './dtos/filter-return.dto';
+import { Return } from './schemas/return.schema';
+import { ApiCreatedResponse, ApiOkResponse, ApiResponse } from '@nestjs/swagger';
 
 @Controller('return')
 export class ReturnController {
   constructor(private readonly returnService: ReturnService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: Return })
   createReturnTicket(@Body() createReturnDto: CreateReturnDto) {
     return this.returnService.createReturnTicket(createReturnDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: [Return] })
   getReturnTicket(@Query() filterReturnDto: FilterReturnDto) {
     return this.returnService.getReturnTicket(filterReturnDto);
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: Return })
   getReturnTicketById(@Param('id') id: string) {
     return this.returnService.getReturnTicketById(id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: Return })
   update(@Param('id') id: string, @Body() updateReturnDto: UpdateReturnDto) {
     return this.returnService.updateReturnTicket(id, updateReturnDto);
   }
 
   @Delete(':id')
+  @ApiResponse({ status: 204, description: 'Delete success' })
   async deleteReturnTicket(@Param('id') id: string): Promise<void> {
     await this.returnService.deleteReturnTicket(id);
   }
