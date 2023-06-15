ecf7524ec6a359cdf1af5ed5df5ddba0a6a8cf38
Gia Hai <haidao549@gmail.com>
2023-06-15 20:41:08 +0700

    creating invoice schema

--- src/invoice/invoice.module.ts
+++ src/invoice/invoice.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { InvoiceService } from './invoice.service';
 import { InvoiceController } from './invoice.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { InvoiceSchema } from './schemas/invoice.schema';
+import { VideoGameModule } from 'src/video-game/video-game.module';
+import { CustomerModule } from 'src/customer/customer.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Invoice', schema: InvoiceSchema }]),
+    VideoGameModule,
+    CustomerModule,
+  ],
   controllers: [InvoiceController],
   providers: [InvoiceService],
 })
