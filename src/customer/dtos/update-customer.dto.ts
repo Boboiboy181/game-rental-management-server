8ab62984bd6c48aa9326a2bbfa510dd52a01f15b
Gia Hai <haidao549@gmail.com>
2023-06-28 14:53:32 +0700

    adding optional validation for point in update customer

--- src/customer/dtos/update-customer.dto.ts
+++ src/customer/dtos/update-customer.dto.ts
@@ -1,10 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCustomerDto } from './create-customer.dto';
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsOptional } from 'class-validator';
 
 export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {
   @ApiPropertyOptional()
   @IsNumber()
+  @IsOptional()
   point?: number;
 }
