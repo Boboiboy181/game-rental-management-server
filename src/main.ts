9303b5b1d0067f5df58124f47b93f04a5545e87f
Gia Hai <haidao549@gmail.com>
2023-07-18 08:03:20 +0700

    remove trailing slash

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173',
-      'https://game-rental-management-client.vercel.app/',
+      'https://game-rental-management-client.vercel.app',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
